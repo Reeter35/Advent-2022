7221087f1df1c031f6ebdf55c01edae2ddcdccd6
ReeterTCN <thibault.cassan@gmail.com>
2022-12-08 21:11:44 +0000

    Day 7 - init

---
+++ Day 7/puzzle1.py
@@ -0,0 +1,70 @@
+
+
+class Node:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.children = []
+        self.files = []
+
+    def getChild(self, name):
+        for child in self.children:
+            if child.name == name:
+                return child
+        return None
+
+    def print(self):
+        s = self.name
+        for f in self.files:
+            s = s + "\n" + str(f)
+        for d in self.children:
+            s = s + "\n" + d.print()
+        return s
+
+    def size(self):
+        s = 0
+        for f in self.files:
+            s = s + int(f[0])
+        for d in self.children:
+            s = s + d.size()
+        return s
+
+  
+def build(read_data):
+    root = Node("", None)
+    node = root
+    for line in read_data:
+        line = line.strip()
+        if line[0] == '$':
+            # Parse command
+            tokens = line.split(' ')
+            if tokens[1] == "cd":
+                # change dir
+                if tokens[2]== "..":
+                    node = node.parent
+                elif node.getChild(tokens[2]) == None:
+                    node.children.append(Node(tokens[2],node))
+                    node = node.getChild(tokens[2])
+                else:
+                    node = node.getChild(tokens[2])
+            #elif tokens[1] == " ls":
+                # list dir
+                # do nothing until next command
+        else:
+            # read the line and add the result to current node
+            tokens = line.split(' ')
+            if tokens[0] == "dir" and node.getChild(tokens[1]) == None:
+                node.children.append(Node(tokens[1], node))
+            else:
+                node.files.append([tokens[0], tokens[1]])
+    
+    directories = []
+    
+
+def run():
+    with open('input') as f:
+        read_data = f.readlines()
+    build(read_data)
+
+if __name__ == '__main__':
+  run()
